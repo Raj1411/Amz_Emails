663d87c9c40e98bc55289ad9d6c8caaea48d62e2
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2022-03-20 20:51:12 +0530

    Add files via upload

---
+++ readEmail.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import imaplib
+import email
+import schedule, time
+from crontab import CronTab
+
+
+
+
+def read_mails():
+    st.title("Email Reader")
+    host='imap.gmail.com'
+    username='rajinder@swissbeauty.in'
+    password='9711609399A'
+
+    mail=imaplib.IMAP4_SSL(host)
+    mail.login(username,password)
+    mail.select('INBOX')
+
+    _,search_data=mail.search(None,'(FROM "supply@meesho.com")','(FROM "supply@meesho.com")',"UNSEEN")
+
+
+    for i in search_data[0].split():
+        _,data=mail.fetch(i,'(RFC822)')
+        email_body=data[0][1]
+    #     # print(email_body)
+        email_body=email.message_from_bytes(email_body)
+        # if email_body['From']=='supply@meesho.com':
+        #     print('From:',email_body['From'])
+        #     print('Subject:',email_body['Subject'])
+        #     print('Body:',email_body.get_payload())
+            # mail.store(i,'+FLAGS','\Seen')
+
+
+if __name__=='__main__':
+    read_mails()
