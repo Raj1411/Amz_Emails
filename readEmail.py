d4be7c6c28265241dd7ddffc4f2c47c59415bc06
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2022-03-20 20:51:40 +0530

    Update readEmail.py

--- readEmail.py
+++ readEmail.py
@@ -2,29 +2,31 @@ import streamlit as st
 import imaplib
 import email
 import schedule, time
-from crontab import CronTab
+# from crontab import CronTab
 
 
+st.title("Email Reader")
 
+btn=st.button('Read Meesho Mails')
 
 def read_mails():
-    st.title("Email Reader")
-    host='imap.gmail.com'
-    username='rajinder@swissbeauty.in'
-    password='9711609399A'
+    if btn:
+        host='imap.gmail.com'
+        username='rajinder@swissbeauty.in'
+        password=st.secrets['KEY']
 
-    mail=imaplib.IMAP4_SSL(host)
-    mail.login(username,password)
-    mail.select('INBOX')
+        mail=imaplib.IMAP4_SSL(host)
+        mail.login(username,password)
+        mail.select('INBOX')
 
-    _,search_data=mail.search(None,'(FROM "supply@meesho.com")','(FROM "supply@meesho.com")',"UNSEEN")
+        _,search_data=mail.search(None,'(FROM "supply@meesho.com")','(FROM "supply@meesho.com")',"UNSEEN")
 
 
-    for i in search_data[0].split():
-        _,data=mail.fetch(i,'(RFC822)')
-        email_body=data[0][1]
+        for i in search_data[0].split():
+            _,data=mail.fetch(i,'(RFC822)')
+            email_body=data[0][1]
     #     # print(email_body)
-        email_body=email.message_from_bytes(email_body)
+            email_body=email.message_from_bytes(email_body)
         # if email_body['From']=='supply@meesho.com':
         #     print('From:',email_body['From'])
         #     print('Subject:',email_body['Subject'])
