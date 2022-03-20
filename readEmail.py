f8c8a1763314710c49ddf28e78b31cd0b472bcd1
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2022-03-20 20:53:13 +0530

    Update readEmail.py

--- readEmail.py
+++ readEmail.py
@@ -19,7 +19,7 @@ def read_mails():
         mail.login(username,password)
         mail.select('INBOX')
 
-        _,search_data=mail.search(None,'(FROM "supply@meesho.com")','(FROM "supply@meesho.com")',"UNSEEN")
+        _,search_data=mail.search(None,'(FROM "do-not-reply@amazon.in")','(FROM "supply@meesho.com")',"UNSEEN")
 
 
         for i in search_data[0].split():
